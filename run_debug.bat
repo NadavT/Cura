a2caaf51529fd7b7895f5b2083dadd35f7397b5b
Nadav Tur <nadavt100@gmail.com>
2022-04-06 20:21:43 +0300

    Copy relevant engines

--- run_debug.bat
+++ run_debug.bat
@@ -1,2 +1,4 @@
-copy /Y "..\CuraEngine\install_dir\bin\CuraEngine.exe" CuraEngine.exe
+copy /Y "..\CuraEngine\install_dir_classic\bin\CuraEngine.exe" CuraEngine.exe
+copy /Y "..\CuraEngine\install_dir_ratio\bin\CuraEngine.exe" CuraEngine_ratio.exe
+copy /Y "..\CuraEngine\install_dir_differential\bin\CuraEngine.exe" CuraEngine_differntial.exe
 cmd /V /C "set PYTHONPATH=!PYTHONPATH!;C:\dev\cura\Uranium && python cura_app.py --debug"
